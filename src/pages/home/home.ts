282ff0daaa084096623e11364628f5688695652a
Prueba <git@github.com:juak134/hhf.git>
2019-01-27 17:17:22 -0500

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -72,17 +72,7 @@ export class HomePage {
   ionViewDidEnter(){
    //Cargando el observable
      this.impri();
-     async function presentLoading() {
-	  const loadingController = document.querySelector('ion-loading-controller');
-	  await loadingController.componentOnReady();
 
-	  const loadingElement = await loadingController.create({
-	    message: 'Please wait...',
-	    spinner: 'crescent',
-	    duration: 2000
-	  });
-	  return await loadingElement.present();
-	}
 }
 
 
